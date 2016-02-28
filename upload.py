0da0b28d838e274906c3a89b2e56cc9a3ea6c883
ngalayko <ngalayko@gmail.com>
2016-02-28 19:06:04 +0300

    now we upload all items till output is empty (now breaking after wow)

--- upload.py
+++ upload.py
@@ -5,23 +5,29 @@ from bs4 import BeautifulSoup
 #ftp parameters
 host = "galayko.netne.net"
 ftp_user = "a8524605"
-ftp_password = ""
+ftp_password = "67636510aA+"
 filename = "./upload.xml"
 time_to_sleep = 30
-files_to_upload = 50
+items_to_upload = 50
 path_to_folder = '/test'
+
+#var for new number of items if we have less than items_to_upload
+n = items_to_upload
 
 while True:
 	#file to get items from
 	source = BeautifulSoup(open('output.xml', 'r').read(), features = 'xml')
 	#file to insert items to
 	target = BeautifulSoup(open('start.xml', 'r').read(), features = 'xml')
-	#if we have enough items
-	if len(source.rss.channel.find_all('item')) < 50:
+	#if we have no items at all
+	if len(source.rss.channel.find_all('item')) == 0:
 		sleep(time_to_sleep )
 		continue
+	#if we have some items, change number of items to upload
+	if len(source.rss.channel.find_all('item')) < 50:
+		n = len(source.rss.channel.find_all('item'))
 	#extracting item from sourse and inserting it to target X 50 times
-	for i in range(files_to_upload):
+	for i in range(n):
 		target.rss.channel.append(source.rss.channel.item.extract())
 	#forming uploading file
 	f = open('upload.xml', 'w')
